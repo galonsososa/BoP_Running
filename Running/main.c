96508e782e291a23030c8a18ec5713e864f52b28
Gonzalo <galonsososa@gmail.com>
2018-11-06 21:44:38 +0100

    update

--- Running/main.c
+++ Running/main.c
@@ -20,6 +20,33 @@ int time_difference(struct tm end,struct tm start){
 
 }
 
+void run_into_file(int day,int month,int year,int start_hour,int start_min,int end_hour,
+                 int end_min,double length){
+    FILE *fp;
+    int status;
+
+    fp = fopen("Running.txt","a"); //file opening in append mode
+    if (fp==NULL) return 1;
+    /*saves into file with format:
+        {
+        DD/MM/YYYY
+        HH:MM-HH:MM
+        d.d
+        }
+    */
+    fprintf(fp,"{\n%02d/%02d/%4d\n%02d:%02d-%02d:%02d\n%1.f\n}\n",
+            day,month,year,
+            start_hour,start_min,end_hour,end_min,
+            length);
+
+
+    status = fclose(fp);
+    if (status!=0){
+     return 1;
+    } else printf("Succesfully saved run into Running.txt\n");
+
+}
+
 double extract_decimals(double num){
 
     int intpart = (int)num;
@@ -27,9 +54,16 @@ double extract_decimals(double num){
     return decpart;
 
 }
+double pace_calculator(int duration,double length){
+    return duration/length;
+}
+
+
 
 int main()
 {
+    //DO YOU WISH TO ADD A FILE OR TO CREATE A NEW ONE?YES->LOADS THE DATA FROM A TEXT FILE;NO->CREATES A NEW TEXT FILE;
+    //case 1 add a run
     Run r1;
     printf("Enter date of the training (DD/MM/YYYY)\n");
     scanf("%d/%d/%d",&r1.start_time.tm_mday
@@ -52,10 +86,16 @@ int main()
     r1.duration = time_difference(r1.end_time,r1.start_time);
     printf("Duration of the training is %.f minutes\n",r1.duration);
 
-    double minutes_per_km = r1.duration/r1.length;
-    r1.pace.tm_min = minutes_per_km;
-    r1.pace.tm_sec = extract_decimals(minutes_per_km) * 60; //converts decimal time into MM:SS format
-    printf("Pace per kilometer is %2d:%2d",r1.pace.tm_min,r1.pace.tm_sec);
+    double pace = pace_calculator(r1.duration,r1.length);
+    r1.pace.tm_min = pace;
+    r1.pace.tm_sec = extract_decimals(pace) * 60; //converts decimal time into MM:SS format
+    printf("Pace per kilometer is %2d:%2d\n",r1.pace.tm_min,r1.pace.tm_sec);
+
+    run_into_file(r1.start_time.tm_mday,r1.start_time.tm_mon,r1.start_time.tm_year,
+                  r1.start_time.tm_hour,r1.start_time.tm_min,r1.end_time.tm_hour,r1.end_time.tm_min,
+                  r1.length);
+
+
 
     return 0;
 
