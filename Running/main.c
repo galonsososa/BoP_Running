5a7d4aba4ad1c3677a48972763ac44e4b09ac422
Gonzalo <galonsososa@gmail.com>
2018-11-06 13:48:33 +0100

    Getting data works

--- Running/main.c
+++ Running/main.c
@@ -2,16 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct date_time DateTime;
-struct run {
+
+typedef struct Run {
     double length;
     struct tm start_time;
     struct tm end_time;
     double duration;
     struct tm pace;
-};
-
-typedef struct run Run;
+}Run;
 
 int time_difference(struct tm end,struct tm start){
 
@@ -19,6 +17,14 @@ int time_difference(struct tm end,struct tm start){
     int minutos = end.tm_min - start.tm_min;
     minutos = minutos + horas*60;
     return minutos;
+
+}
+
+double extract_decimals(double num){
+
+    int intpart = (int)num;
+    double decpart = num - intpart;
+    return decpart;
 
 }
 
@@ -36,16 +42,22 @@ int main()
     //cubrir errores de dias meses erroneos etc
     printf("What time did you start? (HH.MM)\n");
     scanf("%2d.%2d",&r1.start_time.tm_hour,&r1.start_time.tm_min);
+
     printf("What time did you finish? (HH.MM)\n");
     scanf("%2d.%2d",&r1.end_time.tm_hour,&r1.end_time.tm_min);
+
     printf("How many kilometres did you run?\n");
-    scanf("%.1d",&r1.length);
+    scanf("%lf",&r1.length);
+
     r1.duration = time_difference(r1.end_time,r1.start_time);
-    printf("Duration of the training is %.f minutes",r1.duration);
+    printf("Duration of the training is %.f minutes\n",r1.duration);
 
-
+    double minutes_per_km = r1.duration/r1.length;
+    r1.pace.tm_min = minutes_per_km;
+    r1.pace.tm_sec = extract_decimals(minutes_per_km) * 60; //converts decimal time into MM:SS format
+    printf("Pace per kilometer is %2d:%2d",r1.pace.tm_min,r1.pace.tm_sec);
 
     return 0;
-    //asdf
+
 
 }
